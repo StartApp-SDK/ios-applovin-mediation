846a1255ad904498071bf17a6659893ca1cc20ff
Start.io <pavlo.kurliandskyi@start.io>
2022-03-03 19:44:24 +0200

    Adapter release 1.0.0. Added Manual Native ad support. Renamed adapter to StartioAppLovinAdapter.

---
+++ StartioApplovinMediation/StartioAppLovinAdapter.m
@@ -0,0 +1,116 @@
+/**
+ * Copyright 2021 Start.io Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "StartioAppLovinAdapter.h"
+#import "StartioAppLovinAdViewAdapter.h"
+#import "StartioAppLovinInterstitialAdapter.h"
+#import "StartioAppLovinRewardedAdapter.h"
+#import "StartioAppLovinNativeAdapter.h"
+#import <StartApp/StartApp.h>
+
+static NSString * const kAdapterVersion = @"1.0.0";
+
+static NSString * const kAppIdKey = @"app_id";
+
+@interface StartioAppLovinAdapter()<MAAdViewAdapter, MAInterstitialAdapter, MARewardedAdapter, MANativeAdAdapter>
+@property (nonatomic, strong) StartioAppLovinAdViewAdapter *adViewAdapter;
+@property (nonatomic, strong) StartioAppLovinInterstitialAdapter *interstitialAdapter;
+@property (nonatomic, strong) StartioAppLovinRewardedAdapter *rewardedAdapter;
+@property (nonatomic, strong) StartioAppLovinNativeAdapter *nativeAdapter;
+@end
+
+@implementation StartioAppLovinAdapter
+
+- (void)initializeWithParameters:(id<MAAdapterInitializationParameters>)parameters completionHandler:(nonnull void (^)(MAAdapterInitializationStatus, NSString * _Nullable))completionHandler {
+    NSString *appID = parameters.serverParameters[kAppIdKey];
+    if (appID.length != 0) {
+        if ([NSThread isMainThread]) {
+            [self setupStartioSDKWithAppID:appID parameters:parameters];
+            completionHandler(MAAdapterInitializationStatusInitializedSuccess, nil);
+        }
+        else {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                [self setupStartioSDKWithAppID:appID parameters:parameters];
+            });
+            completionHandler(MAAdapterInitializationStatusInitializedSuccess, nil);
+        }
+    }
+    else {
+        completionHandler(MAAdapterInitializationStatusInitializing, @"No Start.io AppID provided yet");
+    }
+}
+
+- (void)setupStartioSDKWithAppID:(NSString *)appID parameters:(id<MAAdapterInitializationParameters>)parameters {
+    [STAStartAppSDK sharedInstance].appID = appID;
+    
+    if (parameters.ageRestrictedUser.boolValue) {
+        STASDKPreferences *prefs = [[STASDKPreferences alloc] init];
+        prefs.age = parameters.ageRestrictedUser.unsignedIntegerValue;
+        [[STAStartAppSDK sharedInstance] setPreferences:prefs];
+    }
+    
+    [[STAStartAppSDK sharedInstance] setUserConsent:parameters.hasUserConsent.boolValue forConsentType:@"pas" withTimestamp:[NSDate timeIntervalSinceReferenceDate]];
+    
+    [[STAStartAppSDK sharedInstance] addWrapperWithName:@"AppLovin" version:kAdapterVersion];
+}
+
+- (NSString *)SDKVersion {
+    return [[STAStartAppSDK sharedInstance] version];
+}
+
+- (NSString *)adapterVersion {
+    return kAdapterVersion;
+}
+
+- (void)destroy {
+    self.adViewAdapter = nil;
+    self.interstitialAdapter = nil;
+    self.rewardedAdapter = nil;
+    self.nativeAdapter = nil;
+}
+
+#pragma mark - MAAdViewAdapter methods
+- (void)loadAdViewAdForParameters:(id<MAAdapterResponseParameters>)parameters adFormat:(MAAdFormat *)adFormat andNotify:(id<MAAdViewAdapterDelegate>)delegate {
+    self.adViewAdapter = [[StartioAppLovinAdViewAdapter alloc] init];
+    [self.adViewAdapter loadAdViewAdapterWithParameters:parameters adFormat:adFormat andNotify:delegate];
+}
+
+#pragma mark - MAInterstitialAdapter methods
+- (void)loadInterstitialAdForParameters:(id<MAAdapterResponseParameters>)parameters andNotify:(id<MAInterstitialAdapterDelegate>)delegate {
+    self.interstitialAdapter = [[StartioAppLovinInterstitialAdapter alloc] init];
+    [self.interstitialAdapter loadInterstitialAdForParameters:parameters andNotify:delegate];
+}
+
+- (void)showInterstitialAdForParameters:(id<MAAdapterResponseParameters>)parameters andNotify:(id<MAInterstitialAdapterDelegate>)delegate {
+    [self.interstitialAdapter showInterstitialAdForParameters:parameters andNotify:delegate];
+}
+
+#pragma mark - MARewardedAdapter methods
+- (void)loadRewardedAdForParameters:(id<MAAdapterResponseParameters>)parameters andNotify:(id<MARewardedAdapterDelegate>)delegate {
+    self.rewardedAdapter = [[StartioAppLovinRewardedAdapter alloc] init];
+    [self.rewardedAdapter loadRewardedAdForParameters:parameters andNotify:delegate];
+}
+
+- (void)showRewardedAdForParameters:(id<MAAdapterResponseParameters>)parameters andNotify:(id<MARewardedAdapterDelegate>)delegate {
+    [self.rewardedAdapter showRewardedAdForParameters:parameters andNotify:delegate];
+}
+
+#pragma mark - MANativeAdAdapter methods
+- (void)loadNativeAdForParameters:(id<MAAdapterResponseParameters>)parameters andNotify:(id<MANativeAdAdapterDelegate>)delegate {
+    self.nativeAdapter = [[StartioAppLovinNativeAdapter alloc] init];
+    [self.nativeAdapter loadNativeAdForParameters:parameters andNotify:delegate];
+}
+@end
---
+++ startio-applovin-mediation.podspec
@@ -0,0 +1,29 @@
+Pod::Spec.new do |spec|
+
+  spec.name         = "startio-applovin-mediation"
+  spec.version      = "1.0.0"
+  spec.summary      = "Start.io <-> AppLovin MAX iOS Mediation Adapter."
+
+  spec.description  = <<-DESC
+  Using this adapter you will be able to integrate Start.io SDK via AppLovin MAX mediation
+                   DESC
+
+  spec.homepage     = "https://www.start.io"
+  spec.license      = { :type => "Apache License, Version 2.0", :file => "LICENSE" }
+  spec.author       = { "iOS Dev" => "iosdev@startapp.com" }
+  
+  spec.platform     = :ios, "10.0"
+  spec.source       = { :git => "https://github.com/StartApp-SDK/ios-applovin-mediation.git", :tag => spec.version.to_s }
+  spec.source_files = "StartioApplovinMediation/*.{h,m}"
+
+  spec.frameworks   = "Foundation", "UIKit"
+
+  spec.requires_arc = true
+  spec.static_framework = true
+
+  spec.user_target_xcconfig = { 'ONLY_ACTIVE_ARCH' => 'YES' } 
+  
+  spec.dependency "AppLovinSDK", "~> 11.1.2"
+  spec.dependency "StartAppSDK", "~> 4.7.0"
+
+end
